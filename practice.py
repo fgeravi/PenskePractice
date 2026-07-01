5b3f3da9a7e6df69ab4dc300a85dcb76a6f97b6e
fgeravi <fgeravi@clemson.edu>
2026-06-30 23:37:01 -0400

    Add average lap times

--- practice.py
+++ practice.py
@@ -5,12 +5,10 @@ with open("race_data.json", "r") as file:
 
 drivers = data["drivers"]
 
-fastest_driver = drivers[0]
+total_time = 0
 
 for driver in drivers:
-    if driver["lap_time"] < fastest_driver["lap_time"]:
-        fastest_driver = driver
+    total_time = total_time + driver["lap_time"]
+    average_time = total_time / len(drivers)
 
-print("Fastest driver: ", fastest_driver["name"])
-print("Lap Time: ", fastest_driver["lap_time"])
-        
+print("Average lap time: ", average_time)
